05da72771d3bed7908441e3a1577e30e2b4163f3
Siddharth <sg8797@gmail.com>
2019-05-19 21:01:10 -0700

    Made misising tile heuristic
    - tested a hardcoded solution from Discord
    - implemented myTiminig() function as well
    - next up: make manhattan heuristic [h2(n)] and max(h1(n),h2(n)) heuristic

--- a1.py
+++ a1.py
@@ -11,7 +11,8 @@
 #	a1.py
 
 from search import *
-import numpy as np    # used in aima-code/aima-python as a library
+import numpy as np    	# used in aima-code/aima-python as a library
+import time   		  	# importing time for timing heuristics
 
 
 
@@ -24,11 +25,12 @@ import numpy as np    # used in aima-code/aima-python as a library
 # make_rand_8puzzle implementation :
 def make_rand_8puzzle():
 	goal=(1, 2, 3, 4, 5, 6, 7, 8, 0)	# this is our final goal to be achieved for every instance of this problem
-	while True:			# Keep generating random initial state till state is a solvable one
-		state = np.random.choice(range(9), 9, replace=False)	# get the random initial state
-		puzzle = EightPuzzle(state, goal)	# create an instance of the problem with the initial state and the end goal
-		if puzzle.check_solvability(puzzle.initial):
-			break
+	# while True:			# Keep generating random initial state till state is a solvable one
+	# 	state = tuple(np.random.choice(range(9), 9, replace=False))	# get the random initial state
+	# 	puzzle = EightPuzzle(state, goal)	# create an instance of the problem with the initial state and the end goal
+	# 	if puzzle.check_solvability(puzzle.initial):
+	# 		break
+	puzzle = EightPuzzle((5,1,6,2,0,3,7,4,8),goal)
 	return puzzle
 
 # display(state) implemetation :
@@ -61,3 +63,32 @@ def display(state):
 #|												|
 #+----------------------------------------------+
 
+def h1(puzzle):
+	x = 0
+	for i in range(9):
+		if puzzle.state[i] != i+1 and puzzle.state[i] != 0:
+			x += 1
+	return x
+
+	
+
+
+def myTiming():
+   start_time = time.time()
+   # goal=(1, 2, 3, 4, 5, 6, 7, 8, 0)
+   # p = EightPuzzle((5,1,6,2,0,3,7,4,8),goal)	# make an instance of problem
+   p = make_rand_8puzzle()
+   display(p.initial)
+   astar_search(p, h1)
+   print(p)
+   elapsed_time = time.time() - start_time
+
+   print('elapsed time (in seconds):'+ str(elapsed_time))
+
+
+# p = make_rand_8puzzle()
+# display(p.initial)
+# print('/n/nrunnning a*search....\n')
+# astar_search(p)
+
+myTiming()
