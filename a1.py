1f62c9bb27b2d74031507405dc9063e212c9cb85
Siddharth <sg8797@gmail.com>
2019-05-21 08:19:14 -0700

    Q2 almost done

--- a1.py
+++ a1.py
@@ -17,7 +17,7 @@ import time   		  	# importing time for timing heuristics
 
 
 #+----------------------------------------------------------------------------------------------------------------------------------------------+
-#| Q1 - Make 2 helper functions:																												|
+#| Q1 - Made 2 helper functions:																												|
 #| 			(i)  make_rand_8puzzle() 	- returns an instance of the EightPuzzle class object with a random initial state that is solvable		|
 #|			(ii) display(state)			- prints the array 'state' in the format of the eight puzzle on the console with 0 being the empty tile	|
 #+----------------------------------------------------------------------------------------------------------------------------------------------+
@@ -25,12 +25,12 @@ import time   		  	# importing time for timing heuristics
 # make_rand_8puzzle implementation :
 def make_rand_8puzzle():
 	goal=(1, 2, 3, 4, 5, 6, 7, 8, 0)	# this is our final goal to be achieved for every instance of this problem
-	# while True:			# Keep generating random initial state till state is a solvable one
-	# 	state = tuple(np.random.choice(range(9), 9, replace=False))	# get the random initial state
-	# 	puzzle = EightPuzzle(state, goal)	# create an instance of the problem with the initial state and the end goal
-	# 	if puzzle.check_solvability(puzzle.initial):
-	# 		break
-	puzzle = EightPuzzle((5,1,6,2,0,3,7,4,8),goal)
+	while True:			# Keep generating random initial state till state is a solvable one
+		state = tuple(np.random.choice(range(9), 9, replace=False))	# get the random initial state
+		puzzle = EightPuzzle(state, goal)	# create an instance of the problem with the initial state and the end goal
+		if puzzle.check_solvability(puzzle.initial):
+			break
+	# puzzle = EightPuzzle((5,1,6,2,0,3,7,4,8),goal)
 	return puzzle
 
 # display(state) implemetation :
@@ -56,34 +56,123 @@ def display(state):
 				row3 += str(state[i]) + " "
 	print(row1 + "\n" + row2 + "\n" + row3 + "\n")
 
-#+----------------------------------------------+
-#| Q2 -											| 
-#|												|
-#|												|
-#|												|
-#+----------------------------------------------+
+#+----------------------------------------------------------------------------------------------------------------------------------+
+#| Q2 -	Created 20 instances of the 8puzzle problem and used the tree algorithms used to solve them and recorded the following data	| 
+#|			- Algorithms used are :																									|
+#|				(1) astar_search() using the h1() function [ h1() function is the misplaced tile heuristic ]						|
+#|				(2) astar_search() using the h2() function [ h2() function is the Manhattan distance heuristic ]					|
+#|				(3) astar_search() using the h3() function [ h3() function is simply the max(h1(),h2()) heuristic ]					|
+#|			- Data recorded for every algortithm listed above:																		|
+#|				i.   Total running time [ seconds ]																					|
+#|				ii.  Length of solution [ path length of reaching from the root node to the gol node ]								|
+#|				iii. Number of nodes removed from frontier 																			|
+#| 																																	|
+#| *****Each algorithm was run on the exact same set of problems to make the comparision unbiassed*****								|
+#+----------------------------------------------------------------------------------------------------------------------------------+
 
-def h1(puzzle):
+
+# MISSING TILE HEURISTIC
+def h1(node):
 	x = 0
 	for i in range(9):
-		if puzzle.state[i] != i+1 and puzzle.state[i] != 0:
+		if node.state[i] != i+1 and node.state[i] != 0:
 			x += 1
 	return x
 
-	
+# 	MANHATTAN HEURISTIC Helper Functions
+def rowCoord(y):
+	return int((x-1)/3)
+
+def colCoord(x):
+	if x % 3 != 0:
+		return x % 3
+	else:
+		return 3
+
+# MANHATTAN HEURISTIC FUNCTION
+def h2(node):
+	mDistances = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+	for i in range(9):
+		if node.state[i] != i+1 and node.state[i] != 0:
+			mDistances[i] = abs(rowCoord(node.state[i] - rowCoord(i+1)) + abs(colCoord(node.state[i]) - colCoord(i+1)))
+	return sum(mDistances)
+
+# Max of Missing tile and Manhattan heuristic FUNCTION
+def h3(node):
+	return max(h1(node),h2(node))
+
+# Creates n instances of the 8puzzle then returns a tuple of them.
+def listOfPuzzles(n):
+	p = []
+	for i in range(n):	# loop creates an instance of a puzzle and runs from i=0 to i=(n-1)
+		instance = make_rand_8puzzle()
+		p.append(instance) # add instance to tuple
+	return p
+
+# USED THESE FUNCTIONS TO GENERATE DATA FOR THE ALGORITHMS. THEY ARE MODIFIED VERSIONS OF astar_search() and best_fit_graph_search() from search.py
+#		MODIFICATION ARE MARKED as the code marked b/w the "# !!! MODIFICATION(n) BY SIDDHARTH GUPTA: Start" and the "# MODIFICATION(n) BY SIDDHARTH GUPTA: End !!!" where n is the nth modification
 
 
-def myTiming():
-   start_time = time.time()
-   # goal=(1, 2, 3, 4, 5, 6, 7, 8, 0)
-   # p = EightPuzzle((5,1,6,2,0,3,7,4,8),goal)	# make an instance of problem
-   p = make_rand_8puzzle()
-   display(p.initial)
-   astar_search(p, h1)
-   print(p)
-   elapsed_time = time.time() - start_time
+# modified the original astar_search() function in search.py file -- !!! USE THIS INSTEAD OF THE search.py version !!!
+def My_astar_search(problem, h=None):
+	"""A* search is best-first graph search with f(n) = g(n)+h(n).
+	You need to specify the h function when you call astar_search, or
+	else in your Problem subclass."""
+	h = memoize(h or problem.h, 'h')
+	return My_best_first_graph_search(problem, lambda n: n.path_cost + h(n))
 
-   print('elapsed time (in seconds):'+ str(elapsed_time))
+# modified the original best_fit_graph_search() function in search.py file -- !!! USE THIS INSTEAD OF THE search.py version !!!
+def My_best_first_graph_search(problem, f):	
+	# Modification made:
+	#	recordes the number of nodes removed and the length of the solution and returns a tuple of the form ( nodesRemoved, length)
+	f = memoize(f, 'f')
+	node = Node(problem.initial)
+	frontier = PriorityQueue('min', f)
+	frontier.append(node)
+	explored = set()
+	#!!! MODIFICATION(1) BY SIDDHARTH GUPTA: Start
+	nodesRemoved = 0
+	length = 0
+	# MODIFICATION(1) BY SIDDHARTH GUPTA: End !!!
+	while frontier:
+		node = frontier.pop()
+		#!!! MODIFICATION(2) BY SIDDHARTH GUPTA: Start
+		nodesRemoved += 1
+# MODIFICATION(2) BY SIDDHARTH GUPTA: End !!!
+		if problem.goal_test(node.state):
+#!!! MODIFICATION(3) BY SIDDHARTH GUPTA: Start
+			return (nodesRemoved,length)
+# MODIFICATION(3) BY SIDDHARTH GUPTA: End !!!
+		explored.add(node.state)
+		for child in node.expand(problem):
+			length += 1
+			if child.state not in explored and child not in frontier:
+				frontier.append(child)
+			elif child in frontier:
+				if f(child) < frontier[child]:
+					del frontier[child]
+					frontier.append(child)
+	return None
+
+
+def getData(n):
+	p = listOfPuzzles(n)
+	# Data for MISSING TILE HEURISTICS
+	i = 1
+	print(" MISSING TILE:\n")
+	for each in p:
+   		start_time = time.time()
+   		d = My_astar_search(each,h1)
+   		elapsed_time = time.time() - start_time
+   		print("===========================================\n")
+   		print("For problem "+str(i)+" :\n")
+   		print("   Time [in seconds]: "+ str(elapsed_time)+"\n")
+   		print("   Nodes removed: " + str(d[0])+"\n")
+   		print("   Length of path: "+ str(d[1])+"\n")
+   		print("===========================================\n")
+   		i += 1
+
+   	
 
 
 # p = make_rand_8puzzle()
@@ -91,4 +180,8 @@ def myTiming():
 # print('/n/nrunnning a*search....\n')
 # astar_search(p)
 
-myTiming()
+s = (5,1,6,2,0,3,7,4,8)
+p = EightPuzzle(s)
+d = My_astar_search(p,h1)
+print("Nodes Removed: "+str(d[0])+"\n")
+print("Steps Taken: "+str(d[1])+"\n")
