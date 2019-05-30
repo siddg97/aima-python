aa769400202b8dc1dd78535b91542d964af66cf5
Siddharth <sg8797@gmail.com>
2019-05-30 16:38:30 -0700

    Update a2_q1.py
    DONE Q1!!!!!

---
+++ a2_q1.py
@@ -0,0 +1,27 @@
+#+----------------------------------------------------------------------------------------------+
+#|Question 1: Erdos-Renyi random graphs:														|
+#|		- Implemented function rand_graph(n,p) which returns a new random graph with n nodes.	|
+#|		- Each node is named from 0 to (n-1).													|
+#|		- every pair of nodes have an edge b/w them according to probability p.					|
+#|		- Assumptions:																			|
+#|			1. n > 1																			|
+#|			2. 0 <= p <= 1																		|
+#|Libraries used:																				|
+#|		1. random module - from the standard python library										|
+#+----------------------------------------------------------------------------------------------+
+
+import random
+
+def rand_graph(n,p):
+	g = dict()	# empty dictionary
+	for i in range(n):	# for every person (named as i) initialize an empty list
+		g[i] = []		
+	for person in range(n-1):	# for every single person in the graph
+		for friend in range(person+1,n): # for every potential friend
+			a = random.random()
+			if a <= p:
+				g[person].append(friend)
+				g[friend].append(person)
+	return g
+
+# END of a2_q1.py
