14181445f5c639c6f8b7272f99ecc65c9bf80959
Siddharth <g.sidd97@gmail.com>
2019-06-22 00:36:18 -0700

    Update a3_q1.py

--- a3_q1.py
+++ a3_q1.py
@@ -1,10 +1,10 @@
 def make_constraints(l,type='atmost_one'):
-	"""generate a string of constraints for the list l which can be a row or column or a diagonal of a n-queens problem."""
-#	USAGE:
-#		- l = list of variables for which constraints are rto be generated
-#		- if type == 'exactly_one' is passed then makes constraints for a row or a column
-#		- if type is not passed OR if type == 'atmost_one' is passed then makes constraints for a diagonal
-
+	"""	generate a string of constraints for the list l which can be a row or column or a diagonal of a n-queens problem.
+		USAGE:
+		- l = list of variables for which constraints are rto be generated
+		- if type == 'exactly_one' is passed then makes constraints for a row or a column
+		- if type is not passed OR if type == 'atmost_one' is passed then makes constraints for a diagonal
+	"""
 	n = len(l)
 	constr = ''
 	for i in range(n-1):
@@ -27,3 +27,12 @@ def make_rows(n):
 		rows.append(r)
 	return rows
 
+def make_cols(n):
+	"""returns a list of columns for the n-queens problem"""
+	cols = []
+	for i in range(1,n+1):
+		c = [i]
+		for j in range(i+n,n*n + 1,n):
+			c.append(j)
+		cols.append(c)
+	return cols
