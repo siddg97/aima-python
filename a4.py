c2df86ef647188271a0e99a552e80cba0d0ac82b
Siddharth <g.sidd97@gmail.com>
2019-07-09 14:59:09 -0700

    Update a4.py

--- a4.py
+++ a4.py
@@ -1,91 +1,104 @@
-# a4.py - START
+def getLegalMoves(board):
+	""" returns a list of all possible legal moves based on the given state """
+	moves = []
+	for i,each in enumerate(board):
+		if each == ' ':
+			moves.append(i+1)
+	return moves
 
-class Board:
-	""" Board object for a 3x3 tic-tac-toe game
-		 + state = a list of 'X', 'O', or '.' values. '.' signifies an empty tile
-		 + moves = a list of all possible moves that can be performed on the given state of the board. A list of integers
-	"""
-	def __init__(self,state):
-		self.state = state
-		self.moves = self.getLegalMoves(state)
+class tictactoe:
+	def __init__(self):
+		self.board = [' ']*9
+		self.nextMoves = [1,2,3,4,5,6,7,8,9]
 
-	def getLegalMoves(self,state):
-		""" returns a list of all possible legal moves based on the given state """
-		moves = []
-		for i,each in enumerate(state):
-			if each == '.':
-				moves.append(i+1)
-		return moves
+	def display(self):
+		state = self.board
+		for i in range(9):
+			r1 = '\x1b[4;30;43m'+ ' '+state[0]+' | '+state[1]+' | '+state[2]+' '+'\x1b[0m' 
+			r2 = '\x1b[4;30;43m'+ ' '+state[3]+' | '+state[4]+' | '+state[5]+' '+'\x1b[0m' 
+			r3 = '\x1b[6;30;43m'+ ' '+state[6]+' | '+state[7]+' | '+state[8]+' '+'\x1b[0m' 
+		print(r1+'\n'+r2+'\n'+r3)
 
-	def applyMove(self,move,token):
-		""" applies the move given to the given state for the token and returns a new state"""
-		new_state = self.state
-		new_state[move-1] = token
-		self.state = new_state
+	def applyMove(self,tile,player):
+		self.board[tile-1]=player
+		self.nextMoves = getLegalMoves(self.board)
 
-	def didEnd(self):
-		""" Returns a 'x' or 'Y' or 'D'
-				+ 'X'     -> player who chose 'X' won
-				+ 'O'     -> player who chose 'O' won
+	def playAsHuman(self,human):
+		while True:
+			h_move = int(input('\x1b[6;30;42m'+'Your next move at?'+'\x1b[0m'))
+			if h_move in self.nextMoves:
+				break
+			else:
+				print("Invalid move! try again!")
+		self.applyMove(h_move,human)
+
+	def playAsComputer(self,computer):
+		c_move = int(input('\x1b[6;30;42m'+'computers next move at?'+'\x1b[0m'))
+		self.applyMove(c_move,computer)
+
+	def outcome(self):
+		""" Returns a 'x' or 'o' or 'D'
+				+ 'x'     -> player who chose 'X' won
+				+ 'o'     -> player who chose 'O' won
 				+ 'D'     -> draw
 				+ 'False' -> game not finished
 		"""
-		state = self.state
-		if (state[0]==state[4] and state[4]==state[8] and state[0] != '.') or (state[2]==state[4] and state[4]==state[6] and state[2] != '.'):
+		state = self.board
+		if (state[0]==state[4] and state[4]==state[8] and state[0] != ' ') or (state[2]==state[4] and state[4]==state[6] and state[2] != ' '):
 			return state[4]
 		else:
 			# check for rows
 			for i in range(0,9,3):
-				if state[i]==state[i+1] and state[i+1]==state[i+2] and state[i] != '.':
+				if state[i]==state[i+1] and state[i+1]==state[i+2] and state[i] != ' ':
 					return state[i]
 			# check for columns
 			for i in range(0,3):
-				if state[i]==state[i+3] and state[i+3]==state[i+6] and state[i] != '.':
+				if state[i]==state[i+3] and state[i+3]==state[i+6] and state[i] != ' ':
 					return state[i]
-			if '.' not in self.state:
+			if ' ' not in self.board:
 				return 'D'
 			else:
 				return False
 
-
-class Tic_tac_toe:
-	def __init__(self,board):
-		self.board = board
-
-	def display(self):
-		state = self.board.state
-		for i in range(9):
-			if state[i] == '.':
-				state[i]=' '
-		r1 = '\x1b[4;30;43m'+ '| '+state[0]+' | '+state[1]+' | '+state[2]+' |'+'\x1b[0m' 
-		r2 = '\x1b[4;30;43m'+ '| '+state[3]+' | '+state[4]+' | '+state[5]+' |'+'\x1b[0m' 
-		r3 = '\x1b[6;30;43m'+ '| '+state[6]+' | '+state[7]+' | '+state[8]+' |'+'\x1b[0m' 
-		print(r1+'\n'+r2+'\n'+r3)
-
 def play_game():
 	s = ['.']*9
-	t =Tic_tac_toe(Board(s))
-	human = input('What token would you like to be [X or O]? ')
+	t = tictactoe()
+	human = input('What token would you like to be [X or O]? ').lower()
+	print('\n')
+	if human=='x':
+		program = 'o'
+	else:
+		program = 'x'
+	while t.outcome()==False:
+		t.display()
+		print('\x1b[6;30;41m'+'Player  : '+human+' '+'\x1b[0m')
+		print('\x1b[6;30;44m'+'Computer: '+program+' '+'\x1b[0m\n')
+		print('\x1b[6;30;47mYour next possible move can be one of the following tiles:\x1b[0m\n\x1b[1;31;40m'+str(t.nextMoves)+'\x1b[0m')
+		t.playAsHuman(human)
+		if len(t.nextMoves) == 0:
+			break
+		t.display()
+		print('\x1b[6;30;41m'+'Player  : '+human+' '+'\x1b[0m')
+		print('\x1b[6;30;44m'+'Computer: '+program+' '+'\x1b[0m\n')
+		print('\n COMPUTERS TURN:')
+		t.playAsComputer(program)
+		if len(t.nextMoves) == 0:
+			break
+	finalstr = 'GAME OVER!'
+	if t.outcome() == human:
+		finalstr += ' HUMAN HAS WON!'
+	elif t.outcome() == program:
+		finalstr += ' COMPUTER HAS WON!'
+	else:
+		finalstr += ' IT IS A TIE!'
 	t.display()
-	print('\x1b[6;30;41m'+'Player: '+human+' '+'\x1b[0m')
+	print(finalstr)
 
-# if __name__ = '__main__':
-# 	play_new_game()
 
 play_game()
-# print(b.didEnd())
-# print(b.state)
-# print(b.moves)
-
-# print(b.didEnd())
-
-# b.applyMove(1,'X')
-# print(b.state)
-# b.applyMove(4,'X')
-# print(b.state)
-
-# b.applyMove(7,'X')
-# print(b.state)
-# print(b.didEnd())
-
-
+# t = tictactoe()
+# t.display()
+# print(t.board)
+# t.applyMove(1,'x')
+# print(t.board)
+# print(t.nextMoves)
